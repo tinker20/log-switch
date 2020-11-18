a1ef9cb84697a66a6c7957a70ae906a75adb80df
Kamal Khatwani <kamalkhatwani69@gmail.com>
2020-11-19 02:15:29 +0530

    Added log-level compatibility with graylog, avoids object instantiation on each call

--- log.js
+++ log.js
@@ -1,32 +1,36 @@
+const LOG_LEVEL = process.env.LOG_LEVEL;
+var grayLogger;
+const graylog2 = require("graylog2");
 /**
  * graylog.emergency
- * graylog.alert
- * graylog.critical
- * graylog.error
- * graylog.warning
- * graylog.notice
- * graylog.info
- * graylog.debug
- */
+ graylog.alert
+ graylog.critical
+ graylog.error
+ graylog.warning
+ graylog.notice
+ graylog.info
+ graylog.debug
+ * */
+const graylogEnums = {
+    alert: 0,
+    critical: 0,
+    notice: 0,
+    emergency: 0,
+    error: 0,
+    warning: 1,
+    info: 2,
+    debug: 3
+};
 
-const graylog2 = require("graylog2");
-const grayLogger = new graylog2.graylog({
-    servers: [
-        { 'host': process.env.GRAYLOG_HOST || 'localhost', port: process.env.GRAYLOG_PORT || '12201' },
-    ],
-    hostname: process.env.GRAYLOG_HOSTNAME || 'tinker20',
-    facility: process.env.GRAYLOG_FACILITY || 'worker',
-    bufferSize: 1350
-});
+grayLogger = grayLogger || new graylog2.graylog({
+        servers: [
+            {'host': process.env.GRAYLOG_HOST || 'localhost', port: process.env.GRAYLOG_PORT || '12201'},
+        ],
+        hostname: process.env.GRAYLOG_HOSTNAME || 'tinker20',
+        facility: process.env.GRAYLOG_FACILITY || 'worker',
+        bufferSize: 1350
+    });
 
-/**
- * error
- * warn
- * info
- * verbose
- * debug
- * silly - Not supported currently.
- */
 
 const winston = require("winston");
 
@@ -43,13 +47,16 @@ const winstonEnums = {
 
 const winstonLogger = winston.createLogger({
     transports: [
-      new winston.transports.Console({level: winstonEnums[process.env.LOG_LEVEL]})
+        new winston.transports.Console({level: winstonEnums[process.env.LOG_LEVEL]})
     ]
-});   
+});
 
-const logger = function(level, message){
-    if(process.env.ENV === 'production'){
-        grayLogger[level](message);
+const logger = function (level, message) {
+    if (process.env.ENV === 'production') {
+        console.log(level, LOG_LEVEL, graylogEnums, graylogEnums[level], graylogEnums[LOG_LEVEL]);
+        if (graylogEnums[level] <= graylogEnums[LOG_LEVEL]) {
+            grayLogger[level](message);
+        }
     }
     else {
         winstonLogger[winstonEnums[level]](message);
